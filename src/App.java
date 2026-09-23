c5453f0913412a4e1cfbdbe2552596b34422cfc2
Leonel Cata <leonel.m.cata03@gmail.com>
2026-09-23 18:26:28 -0300

    feat: obtener string de mensaje mediante un getter

--- src/App.java
+++ src/App.java
@@ -1,4 +1,10 @@
 public class App {
+    private final String mensaje = "Hello, world";
+
+    public String getMensaje() {
+        return this.mensaje;
+    }
+
     public static void main(String[] args) throws Exception {
         String mensaje = "Hello, world";
         System.out.println(mensaje);
