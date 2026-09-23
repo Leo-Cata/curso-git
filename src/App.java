22f11ba6764dff967ba290e897f644581317eede
Dante Puchetta <puchettadante@gmail.com>
2026-09-23 18:53:20 -0300

    aporte de Dante

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String mensaje = "Hello, world";
+        String mensaje = "Hello, Leo!";
         System.out.println(mensaje);
+        
     }
 }
